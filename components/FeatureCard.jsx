035f344be65b096393a7b22db7238351d9006582
shatfield4 <seanhatfield5@gmail.com>
2023-02-08 00:14:31 -0800

    changed ui components

--- components/FeatureCard.jsx
+++ components/FeatureCard.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ArrowButton from "./ArrowButton";
 
-const FeatureCard = ({ bgColor, title, description, onClick }) => (
+const FeatureCard = ({ title, description, handleClick, arrowButtonText }) => {
+    return (
     <div className="w-full lg:w-1/3 p-4">
-      <div className={`bg-${bgColor} rounded-lg p-8 shadow-lg`}>
-        <h3 className="text-xl font-bold text-center text-gray-100 mb-4">
-          {title}
+        <div
+        className={`bg-gradient-to-r from-purple-600 to-orange-400 rounded-lg p-8 shadow-lg`}
+        >
+        <h3 className={`text-xl font-bold text-center text-white mb-4`}>
+            {title}
         </h3>
-        <p className="text-base text-center text-gray-100 mb-8">
-          {description}
+        <p className={`text-base text-center text-white mb-8`}>
+            {description}
         </p>
         <div className="flex justify-center">
-          <ArrowButton text={"Learn More"} onClick={onClick} />
+            <ArrowButton text={"Learn More"} onClick={handleClick} />
         </div>
-      </div>
+        </div>
     </div>
-  );
+    );
+};
 
-    export default FeatureCard;
+export default FeatureCard;
--- components/FeatureSection.jsx
+++ components/FeatureSection.jsx
@@ -3,9 +3,9 @@ import FeatureCard from "./FeatureCard";
 
 const FeatureSection = ({ handleClick }) => (
     <div className="flex flex-wrap items-center justify-center">
-      <FeatureCard bgColor="indigo-600" title="Automated Email Support" description="24/7 email coverage with our AI-powered bot. Let it handle customer inquiries, freeing up your time for more important tasks." onClick={handleClick} />
-      <FeatureCard bgColor="teal-500" title="Shopify Order Management" description="Streamline Shopify order management with our AI support. Keep track of all orders and provide seamless customer experiences." onClick={handleClick} />
-      <FeatureCard bgColor="purple-600" title="Train Your Own VA" description="Personalize customer service with a virtual assistant trained by you. Align responses with your brand voice and style, elevating customer experiences." onClick={handleClick} />
+      <FeatureCard title="Automated Email Support" arrowButtonText={"Learn More"} description="24/7 email coverage with our AI-powered bot. Let it handle customer inquiries, freeing up your time for more important tasks." onClick={handleClick} />
+      <FeatureCard title="Shopify Order Management" arrowButtonText={"Learn More"} description="Streamline Shopify order management with our AI support. Keep track of all orders and provide seamless customer experiences." onClick={handleClick} />
+      <FeatureCard title="Train Your Own VA" arrowButtonText={"Learn More"} description="Personalize customer service with a virtual assistant trained by you. Align responses with your brand voice and style, elevating customer experiences." onClick={handleClick} />
     </div>
   );
 
